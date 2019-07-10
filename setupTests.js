a340d188c944dd3c6038e992702c6717d8ce8246
Kranthi Kumar Pasala <pasala.kk@gmail.com>
2019-07-10 14:41:09 -0400

    Added extra config

--- setupTests.js
+++ setupTests.js
@@ -1,5 +1,3 @@
-import 'babel-polyfill';
-import 'react';
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
--- src/views/LoginView.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-export default class LoginView extends React.Component {
-
-  render() {
-    return <div className="some-class">Hello</div>;
-  }
-}
